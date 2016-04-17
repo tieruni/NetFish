f5cab0d829daff9edcdf3ae257e3808678a8c615
蔚蓝海域 <782068647@qq.com>
2016-04-17 19:17:07 +0800

    页面更新

---
+++ NetFish/NetFish/CellTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  CellTableViewCell.m
+//  NetFish
+//
+//  Created by 蔚蓝海域 on 16/4/17.
+//  Copyright © 2016年 Fish. All rights reserved.
+//
+
+#import "CellTableViewCell.h"
+
+@implementation CellTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+- (IBAction)goodAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
+@end
