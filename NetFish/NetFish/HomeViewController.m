9ed9c9614d4697852094ff6547c24fcf2169d25a
tieruni <1416910544@qq.com>
2016-04-18 19:32:39 +0800

    更新滚动视图

--- NetFish/NetFish/HomeViewController.m
+++ NetFish/NetFish/HomeViewController.m
@@ -8,7 +8,8 @@
 
 #import "HomeViewController.h"
 #import "KSGuideManager.h"
-@interface HomeViewController ()
+#import "WJAdvertCircle.h"
+@interface HomeViewController ()<WJAdvertClickDelegate>
 
 @end
 
@@ -16,6 +17,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self showLaunchAdvert];
     // Do any additional setup after loading the view.
   //引导页
     NSMutableArray *paths = [NSMutableArray new];
@@ -27,6 +29,55 @@
 
     
 
+}
+- (void)showLaunchAdvert{
+    // 创建adview
+    WJAdvertCircle *sss = [[WJAdvertCircle alloc]initWithFrame:CGRectMake(0, 105,self.view.bounds.size.width , self.view.bounds.size.height / 4)];
+    
+    // 设置代理
+    sss.delegate = self;
+    
+    // 参数一：展示的图片名  参数二:点击图片对应的url  参数三:是否可循环重复滚动  参数四:是否设置定时自动滚动 (要定时自动播放 isRepeat的值也必须是YES)
+    [sss showImages:@[@"Image-3",@"Image-1",@"Image-2"] urls:@[@"www.baidu.com",@"www.sina.com.cn",@"www.163.com"] isRepeat:YES isTiming:YES];
+    
+    // pageControl的颜色设置
+    sss.pageControl.currentPageIndicatorTintColor = [UIColor redColor];
+    sss.pageControl.pageIndicatorTintColor = [UIColor blueColor];
+    
+    // 添加视图
+    [self.view addSubview:sss];
+
+}
+// 点击返回的url
+- (void)clickWithUrl:(NSString *)url{
+    
+    NSLog(@"url:%@",url);
+    __block UILabel *lb = [[UILabel alloc]initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 50)];
+    lb.textAlignment = NSTextAlignmentCenter;
+    lb.text = [NSString stringWithFormat:@"点击的url是:%@",url];
+    lb.backgroundColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.5];
+    lb.alpha = 0.0;
+    [UIView animateWithDuration:1 animations:^{
+        lb.alpha = 1;
+    }];
+    [self.view addSubview:lb];
+    
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        [UIView animateWithDuration:1 animations:^{
+            
+            lb.alpha = 0.0;
+            
+        } completion:^(BOOL finished) {
+            
+            [lb removeFromSuperview];
+            
+        }];
+        
+        
+    });
+    
 }
 
 - (void)didReceiveMemoryWarning {
