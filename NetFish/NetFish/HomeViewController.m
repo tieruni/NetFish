5b8e9b9149e1af548685d3d75547c52e85028077
蔚蓝海域 <782068647@qq.com>
2016-04-23 14:00:35 +0800

    111

---
+++ NetFish/NetFish/HomeViewController.m
@@ -0,0 +1,161 @@
+//
+//  HomeViewController.m
+//  NetFish
+//
+//  Created by 蔚蓝海域 on 16/4/17.
+//  Copyright © 2016年 Fish. All rights reserved.
+//
+
+#import "HomeViewController.h"
+#import "KSGuideManager.h"
+#import "WJAdvertCircle.h"
+#import "SlideHeadView.h"
+#import "FirstVC.h"
+@interface HomeViewController ()<WJAdvertClickDelegate>
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self showLaunchAdvert];
+    // Do any additional setup after loading the view.
+  //引导页
+    NSMutableArray *paths = [NSMutableArray new];
+    [paths addObject:[[NSBundle mainBundle] pathForResource:@"10" ofType:@"jpg"]];
+    [paths addObject:[[NSBundle mainBundle] pathForResource:@"11" ofType:@"jpg"]];
+    [paths addObject:[[NSBundle mainBundle] pathForResource:@"12" ofType:@"jpg"]];
+    [paths addObject:[[NSBundle mainBundle] pathForResource:@"13" ofType:@"jpg"]];
+    [[KSGuideManager shared] showGuideViewWithImages:paths];
+    
+    //---------->>>>>>>>>>>>
+    //完成以下步骤即可
+    //初始化SlideHeadView，并加进view
+    SlideHeadView *slideVC = [[SlideHeadView alloc]init];
+    [self.view addSubview:slideVC];
+    
+    //    初始化子控制器，使用-(void)addChildViewController:(UIViewController *)childVC title:(NSString *)vcTitle方法
+    FirstVC *VC1 = [[FirstVC alloc]init];
+    FirstVC *VC2 = [[FirstVC alloc]init];
+    FirstVC *VC3 = [[FirstVC alloc]init];
+    FirstVC *VC4 = [[FirstVC alloc]init];
+    FirstVC *VC5 = [[FirstVC alloc]init];
+    FirstVC *VC6 = [[FirstVC alloc]init];
+    FirstVC *VC7 = [[FirstVC alloc]init];
+    
+    
+    NSArray *titleArr = @[@"热门推荐",@"娱乐",@"军事",@"科技",@"星座",@"生活",@"段子"];
+    slideVC.titlesArr = titleArr;
+    
+    //
+    [slideVC addChildViewController:VC1 title:titleArr[0]];
+    [slideVC addChildViewController:VC2 title:titleArr[1]];
+    [slideVC addChildViewController:VC3 title:titleArr[2]];
+    [slideVC addChildViewController:VC4 title:titleArr[3]];
+    [slideVC addChildViewController:VC5 title:titleArr[4]];
+    [slideVC addChildViewController:VC6 title:titleArr[5]];
+    [slideVC addChildViewController:VC7 title:titleArr[6]];
+    
+    
+    //最后再调用setSlideHeadView  完成
+    [slideVC setSlideHeadView];
+    //----------------------------------
+    
+
+}
+- (void)showLaunchAdvert{
+    // 创建adview
+    WJAdvertCircle *sss = [[WJAdvertCircle alloc]initWithFrame:CGRectMake(0, 105,self.view.bounds.size.width , self.view.bounds.size.height / 4)];
+    
+    // 设置代理
+    sss.delegate = self;
+    
+    // 参数一：展示的图片名  参数二:点击图片对应的url  参数三:是否可循环重复滚动  参数四:是否设置定时自动滚动 (要定时自动播放 isRepeat的值也必须是YES)
+    [sss showImages:@[@"Image-3",@"Image-1",@"Image-2"] urls:@[@"www.baidu.com",@"www.sina.com.cn",@"www.163.com"] isRepeat:YES isTiming:YES];
+    
+    // pageControl的颜色设置
+    sss.pageControl.currentPageIndicatorTintColor = [UIColor redColor];
+    sss.pageControl.pageIndicatorTintColor = [UIColor blueColor];
+    
+    // 添加视图
+    [self.view addSubview:sss];
+
+}
+// 点击返回的url
+- (void)clickWithUrl:(NSString *)url{
+    
+    NSLog(@"url:%@",url);
+    __block UILabel *lb = [[UILabel alloc]initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 50)];
+    lb.textAlignment = NSTextAlignmentCenter;
+    lb.text = [NSString stringWithFormat:@"点击的url是:%@",url];
+    lb.backgroundColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.5];
+    lb.alpha = 0.0;
+    [UIView animateWithDuration:1 animations:^{
+        lb.alpha = 1;
+    }];
+    [self.view addSubview:lb];
+    
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        [UIView animateWithDuration:1 animations:^{
+            
+            lb.alpha = 0.0;
+            
+        } completion:^(BOOL finished) {
+            
+            [lb removeFromSuperview];
+            
+        }];
+        
+        
+    });
+    
+}
+//每次页面出现后
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [[NSNotificationCenter defaultCenter]postNotificationName:@"EnableGesture" object:nil];
+    
+    
+}
+//每次页面消失后
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    [[NSNotificationCenter defaultCenter]postNotificationName:@"DisableGesture" object:nil];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//    
+//    return 1;
+//}
+//
+//
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//   HomeCellTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+//    
+//    // Configure the cell...
+//    
+//    return cell;
+//}
+- (IBAction)menuAction:(UIBarButtonItem *)sender {
+    [[NSNotificationCenter defaultCenter]postNotificationName:@"MenuSwitch" object:nil];
+}
+
+@end
---
+++ NetFish/NetFish/ViewController.m
@@ -0,0 +1,195 @@
+//
+//  ViewController.m
+//  NetFish
+//
+//  Created by tieruni on 16/4/16.
+//  Copyright © 2016年 Fish. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "HomeViewController.h"
+#import <ECSlidingViewController/ECSlidingViewController.h>
+#import "LeftViewController.h"
+#import "XMNAnimTextFiled.h"
+@interface ViewController ()
+
+@property (nonatomic, strong) CAShapeLayer *layer;
+@property (nonatomic, strong)   XMNAnimTextFiled *usernameTF1;
+@property (nonatomic, strong)   XMNAnimTextFiled *passwordTF1;
+@property (strong,nonatomic)ECSlidingViewController *slidingVC;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _usernameTF1 = [[XMNAnimTextFiled alloc] initWithFrame:CGRectMake(20, 100, self.view.frame.size.width - 40, 70)];
+    [_usernameTF1 setPlaceHolderText:@"请输入用户名"];
+    [self.view addSubview:_usernameTF1];
+    
+    
+    _passwordTF1 = [[XMNAnimTextFiled alloc] initWithFrame:CGRectMake(20, 200, self.view.frame.size.width - 40, 70)];
+    [_passwordTF1 setTipsIcon:[UIImage imageNamed:@"invisible_icon"]];
+    [_passwordTF1 setPlaceHolderIcon:[UIImage imageNamed:@"1"]];
+    [_passwordTF1 setPlaceHolderText:@"请输入密码"];
+    [_passwordTF1 setInputType:XMNAnimTextFieldInputTypePassword];
+    [self.view addSubview:_passwordTF1];
+    // Do any additional setup after loading the view, typically from a nib.
+    //初始化一个bool格式的单例化全局变量来表示是否成功执行了注册操作，默认为否
+    //加@转换为number对象
+   [[StorageMgr singletonStorageMgr] addKey:@"SignUpSuccessfully" andValue:@NO];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    //判断是否记忆了用户名
+    if (![[Utilities getUserDefaults:@"Username"]isKindOfClass:[NSNull class]]) {
+        //如果有记忆就把记忆显示在用户名文本输入框中
+        _usernameTF1.text = [Utilities getUserDefaults:@"Username"];
+    }
+}
+
+
+ //每一次这个页面出现的时候都会调用这个方法，并且时机点是页面已经出现以后
+ - (void)viewDidAppear:(BOOL)animated {
+     [super viewDidAppear:animated];
+ //判断是否是从注册页面注册成功后回到的这个登录页面
+     if ([[[StorageMgr singletonStorageMgr]objectForKey:@"SignUpSuccessfully"]boolValue]) {
+ //在自动登录前，将flag恢复为默认的NO
+ [[StorageMgr singletonStorageMgr] removeObjectForKey:@"SignUpSuccessfully"];
+ [[StorageMgr singletonStorageMgr] addKey:@"SignUpSuccessfully" andValue:@NO];
+ //从单例化全局变量中提取用户名和密码
+ NSString *username =[[StorageMgr singletonStorageMgr]objectForKey:@"Username"];
+ NSString *password =[[StorageMgr singletonStorageMgr]objectForKey:@"Password"];
+ //清除用完的用户名和密码
+ [[StorageMgr singletonStorageMgr] removeObjectForKey:@"Username"];
+ [[StorageMgr singletonStorageMgr] removeObjectForKey:@"Password"];
+ //执行登录
+ [self signInWithUsername:username andPassword:password];
+ }
+ }
+-(void)muenuSwitchAction {
+    NSLog(@"菜单");
+    if (_slidingVC.currentTopViewPosition == ECSlidingViewControllerTopViewPositionAnchoredRight) {
+        //上述条件表示中间那扇门正移在右侧，说明门是打开的 因此我们需要将它关闭，也就是将中间的门移回中间
+        [_slidingVC resetTopViewAnimated:YES];
+    }else{
+        //  反之
+        [_slidingVC anchorTopViewToRightAnimated:YES];
+    }
+}
+//激活移门手势
+-(void)enableGestureAction {
+    _slidingVC.panGesture.enabled = YES;
+}
+//关闭移门手势
+-(void)disableGestureAction {
+    _slidingVC.panGesture.enabled = NO;
+}
+
+
+//登录成功后执行的方法
+-(void)popUpHome {
+    
+     //根据故事版的名称和故事版中页面的名称获得这个页面
+   UINavigationController *homeVC = [Utilities getStoryboardInstance:@"Main" byIdentity:@"Home"];
+    //初始化移门的门框,并且同时设置移门中间那扇门
+    _slidingVC = [ECSlidingViewController slidingWithTopViewController:homeVC];
+    //设置开门关门的耗时
+    _slidingVC.defaultTransitionDuration = 0.25f;
+    //设置控制移民开关的手势(这里同时对触摸和拖拽响应)
+    _slidingVC.topViewAnchoredGesture =ECSlidingViewControllerAnchoredGestureTapping |ECSlidingViewControllerAnchoredGesturePanning;
+    //设置手势的识别范围
+    [homeVC.view addGestureRecognizer:_slidingVC.panGesture];
+    
+    //根据故事版中页面的名字获得左滑页面的实例
+    LeftViewController *leftVC = [Utilities getStoryboardInstance:@"Main" byIdentity:@"left"];
+    
+    //设置移民靠左的那扇门
+    _slidingVC.underLeftViewController = leftVC;
+    //设置移门的开闭程度(设置左侧页面当被显示时，宽度能够显示屏幕宽度减去屏幕宽度1/4的宽度值)
+    _slidingVC.anchorRightPeekAmount = UI_SCREEN_W / 4;
+    //创建一个当菜单按钮被按时要执行的侧滑方法的通知
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(muenuSwitchAction) name:@"MenuSwitch" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(enableGestureAction) name:@"EnableGesture" object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(disableGestureAction) name:@"DisableGesture" object:nil];
+    
+    //modal方式跳转到上述页面
+    [self presentViewController:_slidingVC animated:YES completion:nil];
+    
+    
+}
+
+////封装登录操作
+-(void)signInWithUsername:(NSString *)username andPassword:(NSString *)password{
+    //在根视图上创建一朵菊花，并转动
+    UIActivityIndicatorView *avi = [Utilities getCoverOnView:self.view];
+    //开始登录
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * _Nullable user, NSError * _Nullable error) {
+        //登录后的回调
+        [avi stopAnimating];
+        //判断是否登录成功
+        if (user) {
+            NSLog(@"登录成功");
+            //记忆用户名
+            [Utilities setUserDefaults:@"Username" content:username];
+            //将密码文本输入框中的内容清掉
+            _passwordTF1.text = @"";
+            //跳转到首页
+            [self popUpHome];
+        }else{
+            switch (error.code) {
+                case 101:
+                    [Utilities popUpAlertViewWithMsg:@"用户名或密码错误" andTitle:nil onView:self];
+                    break;
+                case 100:
+                    [Utilities popUpAlertViewWithMsg:@"网络不给力，请稍候再试" andTitle:nil onView:self];
+                    break;
+                default:
+                    [Utilities popUpAlertViewWithMsg:@"服务器正在维护，请稍候再试" andTitle:nil onView:self];
+                    break;
+            }
+        }
+    }];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)SignInAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    NSString *username = _usernameTF1.text;
+    NSString *password = _passwordTF1.text;
+    //判断用户是否输入信息
+    if (username.length == 0 || password.length == 0 ) {
+        [Utilities popUpAlertViewWithMsg:@"请填写所有信息" andTitle:nil onView:self];
+        return;
+    }
+    //执行登录
+    [self signInWithUsername:username andPassword:password];
+}
+
+- (IBAction)exitAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+- (IBAction)forgetPWAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
+
+//当键盘按了右下角后会收起键盘
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [textField resignFirstResponder];
+    return YES;
+}
+//按任何地方收键盘
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
+
+
+@end
