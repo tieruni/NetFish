28fb9bb8f4c860834a50b8e3b814f50842cfaa4b
有啥用哦 <1137074620@qq.com>
2016-04-23 11:33:33 +0800

    复位

---
+++ NetFish/NetFish/SignInViewController.h
@@ -0,0 +1,20 @@
+//
+//  SignInViewController.h
+//  NetFish
+//
+//  Created by 蔚蓝海域 on 16/4/16.
+//  Copyright © 2016年 Fish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+- (IBAction)backLoginAction:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UITextField *emailTF;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTF;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTF;
+@property (weak, nonatomic) IBOutlet UITextField *confidPW;
+- (IBAction)SignInUpAction:(UIButton *)sender forEvent:(UIEvent *)event;
+
+@end
---
+++ NetFish/NetFish/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  NetFish
+//
+//  Created by tieruni on 16/4/16.
+//  Copyright © 2016年 Fish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+//@property (weak, nonatomic) IBOutlet UITextField *usernameTF;
+//@property (weak, nonatomic) IBOutlet UITextField *passwordTF;
+- (IBAction)SignInAction:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)exitAction:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)forgetPWAction:(UIButton *)sender forEvent:(UIEvent *)event;
+
+
+
+@end
+
